88ff91033e282f18977012fa7ea5e295aa36ec6c
kapelajii <you@example.com>
2020-06-22 00:12:53 +0300

    setup database and create class for user

--- project/models.py
+++ project/models.py
@@ -1,7 +1,8 @@
-from app import db
+from flask_login import UserMixin
+from . import db
 
-class user(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.string(100))
+    password = db.Column(db.String(100))
     name = db.Column(db.String(500))
--- project/old.py
+++ project/old.py
@@ -1,9 +1,8 @@
-
+""" 
 from decouple import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_login import UserMixin
+from flask_login import LoginManager
 
 
 from main import main as main_blueprint
@@ -11,16 +10,28 @@ from auth import auth as auth_blueprint
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
-db.init_app(app)
+from models import User
+
+# login handling
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader()
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Register blueprints
 app.register_blueprint(main_blueprint)
 app.register_blueprint(auth_blueprint)
 
+
+
+
 if __name__ == '__main__':
-    app.run(debug=config('DEBUG'), host= '0.0.0.0')
+    app.run(debug=config('DEBUG'), host= '0.0.0.0') """
