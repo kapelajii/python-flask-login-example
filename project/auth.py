468e5fd910baa7403c4856b89631d112c616988a
kapelajii <you@example.com>
2020-06-25 02:32:05 +0300

    Created authentication but saving not working

--- project/auth.py
+++ project/auth.py
@@ -1,21 +1,57 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
+from models import User
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
+    
+@auth.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    remember_me = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(username = username).first()
 
-#@auth.route('login', methods=['POST'])
-#def login_post():
+    if not user or check_password_hash(user.password, password):
+        flash('Please check your login details and try again ')
+        return redirect(url_for('auth.login'))
+
+    login_user(user)
+
+    return redirect(url_for('main.user'))
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
-#auth.route('/signup, methods=['POST])
-#def signup_post():
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    name = request.form.get('name')
+
+    user = User.query.filter_by(username = username).first()
+    
+    if user:
+        flash('Username already exist. ')
+        return redirect(url_for('signup.html'))
+        
+    
+    new_user = User(username = username, name = name, password = generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit
+
+    return redirect(url_for('login.html'))
+    
+
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('main.index'))
